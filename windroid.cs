b1ffadcbacacb206c50e9c7f911851bf957898fa
Bhhl <51732503+Bhhl@users.noreply.github.com>
2019-06-12 21:28:47 +1000

    Add files via upload

---
+++ windroid.cs
@@ -0,0 +1,60 @@
+//C:/windroid.cs
+//optimizing CPU technology
+//Optimizing CPU width technology
+//Optimizing CPU power technology
+//Optimizing CPU performance technology
+//Fixed holes for CPU technology
+//Fixed holes for CPU width technology
+//Fixed holes for power technology
+//Fixed holes for performance technology
+//Optimizing GPU technology
+//Optimizing CPU width technology
+//Optimizing power technology
+//Optimizing performance technology
+//Fixed holes for CPU technology
+//Fixed holes for GPU width technology
+//Fixed holes for GPU power technology
+//Fixed holes for GPU performance technology
+//Optimizing RAM technology
+//Optimizing RAM width technology
+//Optimizing RAM power technology
+//Optimizing RAM performance technology
+//Fixed holes for RAM technology
+//Fixed holes for RAM width technology
+//Fixed holes for RAM power technology
+//Fixed holes for RAM performance technology
+//Fixed holes for LED lighting
+//Fixed LED lighting
+//Optimizing LED technology
+//Optimizing LED lighting technology
+//LED 3D lighting technology
+//LED 3D gamma technology
+//LED 3D bridthness technology
+//LED 3D contrast technology
+[System.AttributeUsage(System.AttributeTargets.Windroid, Inherited = false, AllowMultiple = true)]
+sealed class MyAttribute : System.Attribute
+{
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public MyAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+}
+
+
+
